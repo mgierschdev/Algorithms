2fed655c0b195e3f65bed0136c3df4db88626ed5
maximo30 <mgierschdev@gmail.com>
2022-07-13 09:34:11 -0400

    Adding targetSum

---
+++ src/main/java/TargetSum.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+// 494 target sum, leetcode
+public class TargetSum {
+    int count = 0;
+
+    public int findTargetSumWays(int[] nums, int target) {
+        //DFS(0, nums, target, 0);
+
+        int total = Arrays.stream(nums).sum();
+        int[][] dp = new int[nums.length][2 * total + 1];
+
+        dp[0][nums[0] + total] = 1;
+        dp[0][-nums[0] + total] += 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            for (int sum = -total; sum <= total; sum++) {
+                if (dp[i - 1][sum + total] > 0) {
+                    dp[i][sum + nums[i] + total] += dp[i - 1][sum + total];
+                    dp[i][sum - nums[i] + total] += dp[i - 1][sum + total];
+                }
+            }
+        }
+
+        return Math.abs(target) > total ? 0 : dp[nums.length - 1][target + total];
+        // return count;
+    }
+
+    private void DFS(int index, int[] nums, int target, int sum) {
+        if (sum == target && index == nums.length) {
+            count++;
+        }
+
+        if (index >= nums.length) {
+            return;
+        }
+
+        int tmp1 = sum - nums[index];
+        int tmp2 = sum + nums[index];
+
+        DFS(index + 1, nums, target, tmp2);
+        DFS(index + 1, nums, target, tmp1);
+    }
+}
---
+++ src/test/java/TargetSumTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TargetSumTest {
+
+
+    @Test
+    public void TestBaseCase(){
+        TargetSum sum = new TargetSum();
+        Assertions.assertEquals(sum.findTargetSumWays(new int[]{1,1,1,1,1}, 3), 5);
+        Assertions.assertEquals(sum.findTargetSumWays(new int[]{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}, 3), 0);
+    }
+}
