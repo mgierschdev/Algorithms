b548565566d751c07cb8c9aa906f1ee708335729
pacmax2 <mgierschdev@gmail.com>
2022-08-10 14:40:24 -0400

    Refactoring and adding another test case

--- src/main/java/SpiralMatrix.java
+++ src/main/java/SpiralMatrix.java
@@ -1,57 +1,58 @@
 // 54. Leetcode Spiral matrix
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SpiralMatrix {
+    List<Integer> sol;
+    int[][] matrix;
 
-    public List<Integer> spiralOrder(int[][] matrix){
-        List<Integer> sol = new LinkedList<>();
-        int layers = (matrix.length + 1) / 2;
-        int numbers = matrix.length * matrix[0].length;
-//        {1, 2, 3, 4, 5, 6},
-//        {7, 8, 9, 10, 11, 12},
-//        {13, 14, 15, 16, 17, 18}}
-//                {1, 2, 3},
-//                {4, 5, 6},
-//                {7, 8, 9}};
+    public List<Integer> spiralOrder(int[][] matrix) {
+        sol = new ArrayList<>();
+        this.matrix = matrix;
+        int m = matrix.length; // rows
+        int n = matrix[0].length; // cols
+        int layer = 0;
 
-        for(int layer = 0; layer < layers; layer++){
-
-            int n = matrix.length; // rows
-            int m = matrix[0].length; // cols
-
-            int side = n - layer - 1;
-            int side2 = m - layer - 1;
-
-            //up
-            for(int i = layer; i <= side2; i++){
-                sol.add(matrix[layer][i]);
-                numbers--;
-            }
-
-            //right
-            for(int j = layer + 1; j <= side; j++){
-                sol.add(matrix[j][side2]);
-                numbers--;
-            }
-            System.out.println(" "+numbers);
-
-            if(numbers == 0){
-                break;
-            }
-
-            //bot
-            for(int x = side2 - 1; x >= layer ; x--){
-                sol.add(matrix[side][x]);
-                numbers--;
-            }
-
-            //left
-            for(int y = side - 1; y >= layer + 1; y--){
-                sol.add(matrix[y][layer]);
-                numbers--;
-            }
+        while (sol.size() < m * n) {
+            System.out.println("Layer "+layer+" "+sol.size());
+            process(layer, m - 1 - layer, layer, n - 1 - layer); //row1 - row2, col1 - col2
+            layer++;
         }
+
         return sol;
+    }
+
+    public void process(int row1, int row2, int col1, int col2) {
+
+        //top
+        //System.out.println("row1 "+row1+","+row2+" "+sol.size()+" "+col1+","+col2);
+        //System.out.println(" ");
+        for (int i = col1; i <= col2; i++) {
+          //  System.out.println(matrix[row1][i]);
+            sol.add(matrix[row1][i]);
+        }
+
+        if (row1 == row2) {
+            return;
+        }
+
+        //right
+        for (int i = row1 + 1; i <= row2; i++) {
+            sol.add(matrix[i][col2]);
+        }
+
+        if (col1 == col2) {
+            return;
+        }
+
+        //bot
+        for (int i = col2 - 1; i >= col1; i--) {
+            sol.add(matrix[row2][i]);
+        }
+
+        //left
+        for (int i = row2 - 1; i > row1; i--) {
+            sol.add(matrix[i][col1]);
+        }
     }
 }
--- src/test/java/SpiralMatrixTest.java
+++ src/test/java/SpiralMatrixTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
@@ -7,43 +8,48 @@ import java.util.List;
 class SpiralMatrixTest {
 
     @Test
-    public void BaseTest(){
+    public void BaseTest() {
         SpiralMatrix m = new SpiralMatrix();
-        int[][] matrix =new int[][]{
+        int[][] matrix = new int[][]{
                 {1, 2, 3},
                 {4, 5, 6},
                 {7, 8, 9}};
-        Integer[] r = new Integer[]{1,2,3,6,9,8,7,4,5};
-        List<Integer> list = new LinkedList<>();
-        list.addAll(Arrays.asList(r));
+        Integer[] r = new Integer[]{1, 2, 3, 6, 9, 8, 7, 4, 5};
+        List<Integer> list = new LinkedList<>(Arrays.asList(r));
         Assertions.assertEquals(m.spiralOrder(matrix), list);
     }
 
     @Test
     public void
-    UnevenSidesTest(){
+    UnevenSidesTest() {
         SpiralMatrix m = new SpiralMatrix();
-        List<Integer> list = new LinkedList<>();
-        Integer[] r = new Integer[]{1,2,3,4,5,6,12,18,17,16,15,14,13,7,8,9,10,11};
-        int[][] matrix =new int[][]{
+        Integer[] r = new Integer[]{1, 2, 3, 4, 5, 6, 12, 18, 17, 16, 15, 14, 13, 7, 8, 9, 10, 11};
+        int[][] matrix = new int[][]{
                 {1, 2, 3, 4, 5, 6},
                 {7, 8, 9, 10, 11, 12},
                 {13, 14, 15, 16, 17, 18}};
-        list.addAll(Arrays.asList(r));
+        List<Integer> list = new LinkedList<>(Arrays.asList(r));
         Assertions.assertEquals(m.spiralOrder(matrix), list);
     }
 
     @Test
     public void
-    OneSide(){
+    OneSide() {
         SpiralMatrix m = new SpiralMatrix();
-        List<Integer> list = new LinkedList<>();
-        Integer[] r = new Integer[]{1,2,3,4,5,6};
-        int[][] matrix =new int[][]{
+        Integer[] r = new Integer[]{1, 2, 3, 4, 5, 6};
+        int[][] matrix = new int[][]{
                 {1, 2, 3, 4, 5, 6}};
-        list.addAll(Arrays.asList(r));
+        List<Integer> list = new LinkedList<>(Arrays.asList(r));
         Assertions.assertEquals(m.spiralOrder(matrix), list);
     }
 
-
+    @Test
+    public void
+    OutsideBoundsTest() {
+        SpiralMatrix m = new SpiralMatrix();
+        Integer[] r = new Integer[]{1, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 10, 9, 8, 7, 6, 5, 4, 3, 2};
+        int[][] matrix = new int[][]{{1, 11}, {2, 12}, {3, 13}, {4, 14}, {5, 15}, {6, 16}, {7, 17}, {8, 18}, {9, 19}, {10, 20}};
+        List<Integer> list = new LinkedList<>(Arrays.asList(r));
+        Assertions.assertEquals(m.spiralOrder(matrix), list);
+    }
 }
