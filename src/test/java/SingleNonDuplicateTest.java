25bb2a38f5be29d6e16ec7dc7963e27b62fd09aa
pacmax2 <mgierschdev@gmail.com>
2023-01-19 09:42:53 -0400

    Adding SingleNonDuplicate

---
+++ src/test/java/SingleNonDuplicateTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SingleNonDuplicateTest {
+    @Test
+    public void baseCase() {
+        SingleNonDuplicate s = new SingleNonDuplicate();
+        Assertions.assertEquals(s.singleNonDuplicate(new int[] { 1, 1, 2, 3, 3, 4, 4, 8, 8 }), 2);
+        Assertions.assertEquals(s.singleNonDuplicate(new int[] { 3, 3, 7, 7, 10, 11, 11 }), 10);
+    }
+}
