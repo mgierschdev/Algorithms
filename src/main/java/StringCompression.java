737d54125014f826a9264cdeb8a9c58fb0d6f2b6
mgierschdev <mgierschded@gmail.com>
2023-09-28 19:01:29 -0400

    Adding problems

---
+++ src/main/java/StringCompression.java
@@ -0,0 +1,37 @@
+/**
+ @date: September 28, 2023
+ @author: Maximiliam Gierschmann
+ Leetcode reference: https://leetcode.com/problems/string-compression
+
+ Category: Strings, number to string parsing, pointers
+
+ Space/Time Complexity:
+ Space: O(a)
+ Time: O(nodes ^ branching factor)
+ */
+public class StringCompression {
+    public int compress(char[] chars) {
+        int group = 0, res = 0, i = 0;
+
+        while(i < chars.length){
+            group = 1;
+
+            while(group + i < chars.length &&
+                    chars[i] == chars[i + group]){
+                group++;
+            }
+
+            chars[res++] = chars[i];
+
+            if(group > 1){
+                for(char c : Integer.toString(group).toCharArray()){
+                    chars[res++] = c;
+                }
+            }
+
+            i += group;
+        }
+
+        return res;
+    }
+}
