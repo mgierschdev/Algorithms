57356f067d5351d8629bbc4067fcc872a79c628c
mgierschdev <mgierschded@gmail.com>
2023-09-29 22:23:53 -0400

    Fixing unit tests

--- src/main/java/Array/KDiffPairs.java
+++ src/main/java/Array/KDiffPairs.java
@@ -4,14 +4,14 @@ import java.util.HashMap;
 /**
  @date: October 3, 2020
  @author: Maximiliam Gierschmann
- Leetcode reference: https://leetcode.com/problems/k-diff-pairs-in-an-array/
+ Leetcode reference: <a href="https://leetcode.com/problems/k-diff-pairs-in-an-array/">...</a>
 
  Space/Time Complexity:
  Space: O(n): where n is the size of the input
  Time: O(n): where n is the size of the input
 
  Constraints:
- 1 <= nums.length <= 10^4
+ 1 <= nums. Length <= 10^4
  -10^7 <= nums[i] <= 10^7
  0 <= k <= 107
  */
@@ -31,7 +31,6 @@ public class KDiffPairs {
                     map.put(i, map.get(i) + 1);
                     count++;
                 }
-                continue;
             }else{
 
                 count += map.getOrDefault(i - k, 0);
