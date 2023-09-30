25a9d440418a3b9f1771e1e439d1df552c6cca9d
mgierschdev <mgierschded@gmail.com>
2023-09-29 22:27:25 -0400

    Adding problems

---
+++ src/main/java/StackQueue/RemoveStars.java
@@ -0,0 +1,35 @@
+package StackQueue;
+
+import java.util.Stack;
+
+/**
+ @date: September 28, 2023
+ @author: Maximiliam Gierschmann
+ Leetcode reference: https://leetcode.com/problems/removing-stars-from-a-string/
+
+ Category: Strings, stack
+ */
+
+public class RemoveStars {
+    public String removeStars(String s) {
+        Stack<Character> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+
+        for(char c : s.toCharArray()){
+
+            if(c != '*'){
+                stack.push(c);
+            }else{
+                stack.pop();
+            }
+        }
+
+        while(!stack.isEmpty()){
+            sb.append(stack.pop());
+        }
+
+        sb.reverse();
+
+        return sb.toString();
+    }
+}
